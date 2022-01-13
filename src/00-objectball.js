b435b90c72e92eddbc19ee74ae16639be777feb2
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-01-13 08:57:51 -0600

    add gameObject obj

---
+++ src/00-objectball.js
@@ -0,0 +1,24 @@
+const gameObject = {
+    home:{
+        teamName: ,
+        colors: ,
+        players: {
+            alanAnderson: {
+                number: 0,
+                shoe: 16,
+                points: 22,
+                rebounds: 12,
+                assists: 12,
+                steals: 3,
+                blocks: 1,
+                slamDunks: 1
+            }
+
+        },
+    }, 
+    away:{
+        teamName: ,
+        colors: ,
+        players: ,
+    },
+}
